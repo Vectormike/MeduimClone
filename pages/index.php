09fdd0659f1ed4aa206713af78d027ca73ce5ae6
Victor Jonah <victor.junior40@yahoo.com>
2018-08-20 12:45:19 +0100

    fixed sign modal form

---
+++ pages/index.php
@@ -0,0 +1,181 @@
+<? php
+session_start();
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Medium – a place to read and write big ideas and important stories</title>
+  </head>
+  <body>
+    <div class="full-page">
+        <div class="site-main">
+            <nav class="navbar navbar-light bg-light justify-content-between">
+                <a href="#">Become a member</a>
+                <span class="svgIcon svgIcon--logoWordmark svgIcon--138x27px is-flushLeft u-xs-hide u-textColorDarker"><svg class="svgIcon-use" width="138" height="27" viewBox="0 0 138 27"><path d="M130 27V16.96c0-3.26-.154-5.472-2.437-5.472-1.16 0-2.138.57-2.863 1.512.217.906.3 1.968.3 3.127 0 2.247.036 5.11 0 7.973 0 .472-.046.58.244.87L127 27h-8V16.96c0-3.297-.461-5.472-2.708-5.472-1.16 0-1.64.653-2.292 1.595V24.1c0 .472-.026.58.3.87L116 27h-8V11.56c0-.47-.07-.579-.36-.905L106 9h8v3.612c.906-2.537 2.437-4.112 5.372-4.112 2.682 0 4.494 1.466 5.255 4.257.834-2.392 3.008-4.257 6.053-4.257 3.588 0 5.32 2.626 5.32 7.627 0 2.392.036 5.11 0 7.973 0 .472.004.652.25.87L138 27h-8zm-27-3.045c0 .472-.149.617.178.906L105 27h-8v-4c-.906 2.465-2.956 4-5.637 4C87.775 27 86 24.39 86 19.461c0-2.391-.036-5 0-7.936 0-.471-.11-.58-.4-.87L84 9h8v9.628c0 3.225.269 5.4 2.298 5.4 1.16 0 2.086-.725 2.702-1.63V11.56c0-.471-.129-.58-.419-.906L95 9h8v14.955zM78.002.25A3.248 3.248 0 0 1 81.25 3.5 3.25 3.25 0 1 1 78.002.25zM75 27V11.56c0-.47.168-.579-.122-.905L73 9h8v15.1c0 .472-.01.678.24.9L83 27h-8zM64 11.706c-.507-.652-1.418-1.123-2.396-1.123-1.957 0-3.842 1.775-3.842 7.03 0 4.93 1.631 6.669 3.66 6.669.907 0 1.853-.436 2.578-1.378V11.706zm6 12.286c0 .47-.026.58.3.87L72 27h-8v-3.697C62.913 25.804 60.951 27 58.632 27 54.5 27 51.5 23.738 51.5 17.795c0-5.582 3.254-9.314 7.784-9.314 2.356 0 3.919 1.123 4.716 2.899V3.878c0-.471-.077-.617-.403-.906L62 1.305 70 .29v23.702zM43.9 16c.037-.471.037-.67.037-.815 0-4.747-.937-5.435-2.437-5.435-1.5 0-2.854.995-2.927 6.25h5.328zm-5.327 1c0 4.711 2.392 6.63 5.183 6.63 2.174 0 4.313-.943 5.509-3.335h.072c-.942 4.566-3.77 6.705-8.01 6.705-4.566 0-8.879-2.755-8.879-9.133 0-6.705 4.277-9.386 9.097-9.386 3.842 0 7.937 1.811 7.937 7.646 0 .109 0 .438-.036.873H38.573zM31.5 27h-12l2.39-2.646c.084-.084.11-.399.11-.87V7l-7.866 20L5.581 8.372C5.364 7.9 5.181 7.285 5 6.777V20.62c0 .58-.035.653.364 1.196L9 27H0l3.64-5.183c.399-.543.36-.616.36-1.196V6.27c0-.617.095-.69-.195-1.051L1 1h8.495l7.355 16.3L23.24 1h8.26l-2.2 2.75c-.326.326-.3.599-.3 1.106v18.629c0 .47.005.75.138.87L31.5 27z"></path></svg></span>
+                <form class="form-inline">
+                    <a class="button button--small button--chromeless u-sm-show is-inSiteNavBar u-baseColor--buttonNormal button--withIcon button--withSvgIcon u-xs-top2 is-touched" href="https://medium.com/search" title="Search" aria-label="Search"><span class="button-defaultState"><span class="svgIcon svgIcon--search svgIcon--25px"><svg class="svgIcon-use" width="25" height="25" viewBox="0 0 25 25"><path d="M20.067 18.933l-4.157-4.157a6 6 0 1 0-.884.884l4.157 4.157a.624.624 0 1 0 .884-.884zM6.5 11c0-2.62 2.13-4.75 4.75-4.75S16 8.38 16 11s-2.13 4.75-4.75 4.75S6.5 13.62 6.5 11z"></path></svg></span></span></a>
+                    <div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header text-center">
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div>
+                                        <h1 class="modal-title w-100 font-weight-bold text-center">Welcome Back</h1>    
+                                        <h2 class="signInInformation text-center">Sign in to access your personalized homepage, follow authors and topics you love, and clap for stories that matter to you.</h2>
+                                            <section class="loginOption text-center">
+                                                <label>Username:</label>
+                                                <br>
+                                                <input type="text" name="username" id="username" class="form-control">
+                                                <label>Password:</label>
+                                                <input type="password" name="password" id="password" class="form-control">   
+                                                <br>
+                                                <button type="button" class="btn btn-primary" id="loginbutton" name="loginbutton"></button>                                     
+                                            </section> 
+                                        <div class="text-center">
+                                                <span class="ui-body">No account?</span> 
+                                            <button class="button button--primary button--chromeless u-accentColor--buttonNormal">Create one</button>.
+                                            <div class="ui-body u-maxWidth400 u-marginAuto u-marginTop15">To make Medium work, we log user data and share it with service providers. Click "Sign in" above to accept Medium’s 
+                                                <a class="ds-link ds-link--styleObvious" href="#" target="_blank">Terms of Service</a> &amp; 
+                                                <a class="ds-link" href="#" target="_blank">Privacy Policy</a>.
+                                            </div>
+                                        </div>                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <div class="text-center">
+                    <a href="" class="btn btn-default btn-rounded mb-4" data-toggle="modal" data-target="#modalLoginForm">Sign in</a>
+                    </div>
+                    <div class="">
+                        <button class="btn btn-outline-success my-2 my-sm-0" data-target="#modalRegisterForm" data-toggle="modal" type="button">Get Started</button>
+                    </div>
+                        <div class="modal fade" id="modalRegisterForm" tabindex="-1" role="dialog" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <section class="text-center">
+                                            <button class="button button--close button--chromeless" data-action="overlay-close">
+                                                <span class="svgIcon svgIcon--removeThin svgIcon--29px">
+                                                    <svg class="svgIcon-use" width="29" height="29" viewBox="0 0 29 29">
+                                                        <path d="M20.13 8.11l-5.61 5.61-5.609-5.61-.801.801 5.61 5.61-5.61 5.61.801.8 5.61-5.609 5.61 5.61.8-.801-5.609-5.61 5.61-5.61" fill-rule="evenodd"></path>
+                                                    </svg>
+                                                </span>
+                                            </button>
+                                            <h1 class="ui-brand2 u-maxWidth600 u-marginAuto u-marginBottom20 js-susiTitle">Join Medium.</h1>
+                                            <h2 class="ui-body">Create an account to personalize your homepage, follow your favorite authors and publications, applaud stories you love, and more.</h2>
+                                            <div class="">
+                                                <button class="button button--large button--borderless button--withChrome u-baseColor--buttonNormal button--withIcon button--withSvgIcon button--withIconAndLabel u-boxShadow u-textAlignLeft u-marginBottom15 u-backgroundWhite u-textColorDarker u-sm-width220 u-sm-marginRight20 u-xs-marginRight0 u-sizeFullWidth js-googleButton" data-action="google-auth" data-action-value="register" data-action-source="--------------------------nav_reg" data-redirect="https://medium.com/?source=--------------------------nav_reg">
+                                                    <span class="svgIcon svgIcon--googleNew svgIcon--25px">
+                                                        <svg class="svgIcon-use" width="25" height="25" viewBox="0 0 25 25">
+                                                            <g fill="none" fill-rule="evenodd">
+                                                                <path d="M20.66 12.693c0-.603-.054-1.182-.155-1.738H12.5v3.287h4.575a3.91 3.91 0 0 1-1.697 2.566v2.133h2.747c1.608-1.48 2.535-3.65 2.535-6.24z" fill="#4285F4"></path>
+                                                                <path d="M12.5 21c2.295 0 4.22-.76 5.625-2.06l-2.747-2.132c-.76.51-1.734.81-2.878.81-2.214 0-4.088-1.494-4.756-3.503h-2.84v2.202A8.498 8.498 0 0 0 12.5 21z" fill="#34A853"></path>
+                                                                <path d="M7.744 14.115c-.17-.51-.267-1.055-.267-1.615s.097-1.105.267-1.615V8.683h-2.84A8.488 8.488 0 0 0 4 12.5c0 1.372.328 2.67.904 3.817l2.84-2.202z" fill="#FBBC05"></path>
+                                                                <path d="M12.5 7.38c1.248 0 2.368.43 3.25 1.272l2.437-2.438C16.715 4.842 14.79 4 12.5 4a8.497 8.497 0 0 0-7.596 4.683l2.84 2.202c.668-2.01 2.542-3.504 4.756-3.504z" fill="#EA4335"></path>
+                                                            </g></svg>
+                                                        </span>
+                                                        <span class="button-label  js-buttonLabel">Sign up with Google</span>
+                                                </button>
+                                                <button class="button button--large button--borderless button--withChrome" data-action="facebook-auth">
+                                                    <span class="svgIcon svgIcon--facebookSquare svgIcon--25px svgIcon--facebookBlue">
+                                                        <svg class="svgIcon-use" width="25" height="25" viewBox="0 0 25 25">
+                                                            <path d="M20.292 4H4.709A.709.709 0 0 0 4 4.708v15.584c0 .391.317.708.709.708h8.323v-6.375h-2.125v-2.656h2.125V9.844c0-2.196 1.39-3.276 3.348-3.276.938 0 1.745.07 1.98.1v2.295h-1.358c-1.066 0-1.314.507-1.314 1.25v1.756h2.656l-.531 2.656h-2.125L15.73 21h4.562a.708.708 0 0 0 .708-.708V4.708A.708.708 0 0 0 20.292 4" fill-rule="evenodd"></path>
+                                                        </svg>
+                                                    </span>
+                                                    <span class="button-label  js-buttonLabel">Sign up with Facebook</span>
+                                                </button>
+                                            </div>
+                                            <span class="ui-body">Already have an account?</span>
+                                            <button class="button button--primary" data-action="sign-in-prompt">Sign in</button>
+                                            <div class="ui-body">To make Medium work, we log user data and share it with service providers. Click "Sign up" above to accept Medium’s <a class="ds-link" href="" target="_blank">Privacy Policy</a>.</div>
+                                    </section>
+                                </div>
+                            </div>
+                        </div>
+                </form>
+            </nav>
+            <div class="">
+                <nav class="ds-nav ds-nav--textCaps ds-nav--spacingNone js-navBar" data-action-scope="_actionscope_3">
+                    <div class="u-flexCenter">
+                        <div class="">
+                            <button class="button button--chromeless u-baseColor--buttonNormal button--withIcon button--withSvgIcon" data-action="scroll-carousel" data-action-value="-1" disabled="disabled"
+                                <span class="svgIcon svgIcon--arrowLeft svgIcon--21px is-flushLeft">
+                                    <svg class="svgIcon-use" width="21" height="21" viewBox="0 0 21 21">
+                                        <path d="M13.402 16.957l-6.478-6.479L13.402 4l.799.71-5.768 5.768 5.768 5.77z" fill-rule="evenodd">
+                                        </path>
+                                    </svg>
+                                </span>
+                            </button>
+                        </div>
+                    <div class="u-flex1 u-overflowHidden">
+                        <div class="u-flexTop">
+                            <li class="ds-nav-item js-tabsItem is-active"><span class="ds-nav-text"><a class="ds-link ds-link--stylePointer" href="#">Home</a></span></li>
+                            <li class="ds-nav-item js-tabsItem"><span class="ds-nav-text"><a class="ds-link" href="#">Great Escape</a></span></li>
+                            <li class="ds-nav-item js-tabsItem"><span class="ds-nav-text"><a class="ds-link" href="#">Culture</a></span></li>
+                            <li class="ds-nav-item js-tabsItem"><span class="ds-nav-text"><a class="ds-link" href="#">Tech</a></span></li>
+                            <li class="ds-nav-item js-tabsItem"><span class="ds-nav-text"><a class="ds-link" href="#">Entrepreneurship</a></span></li>
+                            <li class="ds-nav-item js-tabsItem"><span class="ds-nav-text"><a class="ds-link" href="#">Self</a></span></li>
+                            <li class="ds-nav-item js-tabsItem"><span class="ds-nav-text"><a class="ds-link" href="#">Politics</a></span></li>
+                            <li class="ds-nav-item js-tabsItem"><span class="ds-nav-text"><a class="ds-link" href="#">Design</a></span></li>
+                            <li class="ds-nav-item js-tabsItem"><span class="ds-nav-text"><a class="ds-link" href="#">Science</a></span></li>
+                            <li class="ds-nav-item js-tabsItem"><span class="ds-nav-text"><a class="ds-link" href="#">Popular</a></span></li>
+                            <li class="ds-nav-item js-tabsItem"><span class="ds-nav-text"><a class="ds-link" href="#">More</a></span></li>
+                        </div>
+                    </div>
+                        <div class="u-flex0 u-marginLeftAuto u-paddingLeft10 js-carouselNavigation u-hide">
+                           <button class="button button--chromeless u-baseColor--buttonNormal button--withIcon button--withSvgIcon js-carouselForward" data-action="scroll-carousel">
+                                <span class="svgIcon svgIcon--arrowRight svgIcon--21px is-flushRight">
+                                    <svg class="svgIcon-use" width="21" height="21" viewBox="0 0 21 21">
+                                        <path d="M8.3 4.2l6.4 6.3-6.4 6.3-.8-.8 5.5-5.5L7.5 5" fill-rule="evenodd"></path>
+                                    </svg>
+                                  </span>
+                            </button>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <div>
+      
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+  <div id="fb-root"></div>
+<script>
+    $(document).ready(function(){
+        $(loginbutton).click(function(){
+            var username = $('#username').val()
+            var password = $('#password').val()
+            if(username = ' ' || password != ' ') && (username ! = ' ' || password = ' ' ){
+                alert("Both fields are required");
+                }
+            else if{
+                $.ajax({
+                   alert("Username and Password is required");
+                }
+            else {
+                $.ajax({
+                    url:"login.php",
+                    method:"POST",
+                    data:{username:username, password:password},
+                    success:function
+                });
+            }    
+        });
+    )};
+</script>
+</html>
